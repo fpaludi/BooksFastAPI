45cec052262698990b0b4fa73371f8abcb953335
fpaludi <francopaludi2@gmail.com>
2022-01-29 15:35:28 -0300

    Add logger

--- app/src/db/session.py
+++ app/src/db/session.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from settings import settings
+from logger import get_logger
+
+logger = get_logger("db_session")
 
 engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+logger.info("Create session")
